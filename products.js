ca9e4846a061001af8f7ab8368e595dcc55923d9
ratonbienyvos-beep <ratonbienyvos@gmail.com>
2026-08-20 13:18:05 -0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,58 @@
+const PRODUCTS = [
+  {
+    "cat": "beleza",
+    "title": "Kokeshi Adeus Olheiras 10ml",
+    "desc": "Sérum roll-on para a área dos olhos, prático para incluir na rotina diária de skincare.",
+    "img": "assets/kokeshi-olheiras.png",
+    "url": "https://s.shopee.com.br/40frnQuSMw",
+    "tag": "Área dos olhos"
+  },
+  {
+    "cat": "beleza",
+    "title": "Kit Kokeshi Pele de Porcelana + Geleia de Colágeno",
+    "desc": "Dois cuidados em uma rotina prática de limpeza e hidratação facial.",
+    "img": "assets/kokeshi-pele-porcelana.png",
+    "url": "https://s.shopee.com.br/qiq1SSHre",
+    "tag": "Kit skincare"
+  },
+  {
+    "cat": "beleza",
+    "title": "Kit Facial Kokeshi",
+    "desc": "Creme facial Pele de Porcelana + cuidado específico para a área dos olhos.",
+    "img": "assets/kokeshi-kit-facial.png",
+    "url": "https://s.shopee.com.br/19j1r6XYu",
+    "tag": "Rosto + olhos"
+  },
+  {
+    "cat": "moda",
+    "title": "Vestido Midi Laranja Feminino",
+    "desc": "Uma peça vibrante e elegante para produções leves, modernas e cheias de personalidade.",
+    "img": "assets/vestido-laranja.png",
+    "url": "https://onelink.shein.com/48/5z01fxpi3ayy",
+    "tag": "Vestidos"
+  },
+  {
+    "cat": "moda",
+    "title": "Vestido Midi Preto com Detalhes Dourados",
+    "desc": "Clássico, sofisticado e fácil de combinar em diferentes ocasiões.",
+    "img": "assets/vestido-preto.png",
+    "url": "#",
+    "tag": "Vestidos"
+  },
+  {
+    "cat": "moda",
+    "title": "Vestido Longo Lilás com Babados",
+    "desc": "Elegância feminina para festas, casamentos e ocasiões especiais.",
+    "img": "assets/vestido-lilas.png",
+    "url": "https://onelink.shein.com/48/5z02fcbgtcep",
+    "tag": "Festa"
+  },
+  {
+    "cat": "moda",
+    "title": "Calça Feminina com Lantejoulas e Franjas",
+    "desc": "Brilho e movimento para festas, festivais e noites especiais.",
+    "img": "assets/calca-lantejoulas.png",
+    "url": "https://onelink.shein.com/48/5zd5dr17hl44?ismg_ol=KLQLY9ztmuX_01_KOC-C",
+    "tag": "Festa"
+  }
+];
